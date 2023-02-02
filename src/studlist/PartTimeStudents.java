835d14d509b45ee2e527b70d8ca41bb92ca4bde1
Abhay <abhayab@sheridancollege.ca>
2023-02-02 14:59:52 -0500

    my first commit

---
+++ src/studlist/PartTimeStudents.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studlist;
+
+/**
+ *
+ * @author 91816
+ */
+public class PartTimeStudents {
+    private int numOfHours;
+    
+}
